62a9f490b6749024c06aefb85465621faff1e241
jampsy-89 <kodaikanaloct2023>
2024-09-18 21:27:44 +0530

    details

--- start.py
+++ start.py
@@ -1,4 +1,18 @@
-print("welcome to ")
-age = int(input("32"))
-name_ = input("enter the name:")
-car = input("car1")
+#print("welcome to ")
+#age = int(input("32"))
+#name_ = input("enter the name:")
+#car = input("car1")
+
+### input and output satements
+
+#jam = int(input("enter the number from user:"))
+#butter = jam**2
+#if butter >=0:
+    #print(f"the square value:{butter}")
+#else:
+    #print(f"not sqaure value")
+    
+#sab = input("Enter the name:") 
+#reverse1 = sab[::-1]
+print(f"The reversed format:{reverse1}") 
+
